93e8559b5a3b524c95fe67cfbe8fcb8bafe0a0d3
williamwebster1 <37878196+williamwebster1@users.noreply.github.com>
2019-03-04 14:03:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+
+function nextImageR1(el){
+if (el.src.match("image/red1.jpg")){
+el.src = "image/red1r1.jpg";
+} else if (el.src.match("image/red1r1.jpg")){
+el.src = "image/red1r2.jpg";
+} else if (el.src.match("image/red1r2.jpg")){
+el.src = "image/red1r3.jpg";
+} else if (el.src.match("image/red1r3.jpg")){
+el.src = "image/red1.jpg";
+} else {
+}
+}
+
+function nextImageR2(el){
+if (el.src.match("image/red2.jpg")){
+el.src = "image/red2r1.jpg";
+} else if (el.src.match("image/red2r1.jpg")){
+el.src = "image/red2r2.jpg";
+} else if (el.src.match("image/red2r2.jpg")){
+el.src = "image/red2r3.jpg";
+} else if (el.src.match("image/red2r3.jpg")){
+el.src = "image/red2.jpg";
+} else {
+}
+}
+
+function nextImageR3(el){
+if (el.src.match("image/red3.jpg")){
+el.src = "image/red3r1.jpg";
+} else if (el.src.match("image/red3r1.jpg")){
+el.src = "image/red3r2.jpg";
+} else if (el.src.match("image/red3r2.jpg")){
+el.src = "image/red3r3.jpg";
+} else if (el.src.match("image/red3r3.jpg")){
+el.src = "image/red3.jpg";
+} else {
+}
+}
+
+function nextImageB1(el){
+if (el.src.match("image/blue1.jpg")){
+el.src = "image/blue1r1.jpg";
+} else if (el.src.match("image/blue1r1.jpg")){
+el.src = "image/blue1r2.jpg";
+} else if (el.src.match("image/blue1r2.jpg")){
+el.src = "image/blue1r3.jpg";
+} else if (el.src.match("blue1r3.jpg")){
+el.src = "image/blue1.jpg";
+} else {
+}
+}
+
+function nextImageB2(el){
+if (el.src.match("image/blue2.jpg")){
+el.src = "image/blue2r1.jpg";
+} else if (el.src.match("image/blue2r1.jpg")){
+el.src = "image/blue2r2.jpg";
+} else if (el.src.match("image/blue2r2.jpg")){
+el.src = "image/blue2r3.jpg";
+} else if (el.src.match("image/blue2r3.jpg")){
+el.src = "image/blue2.jpg";
+} else {
+}
+}
+
+function nextImageB3(el){
+if (el.src.match("image/blue3.jpg")){
+el.src = "image/blue3r1.jpg";
+} else if (el.src.match("image/blue3r1.jpg")){
+el.src = "image/blue3r2.jpg";
+} else if (el.src.match("image/blue3r2.jpg")){
+el.src = "image/blue3r3.jpg";
+} else if (el.src.match("image/blue3r3.jpg")){
+el.src = "image/blue3.jpg";
+} else {
+}
+}
+
+function nextImageG1(el){
+if (el.src.match("image/green1.jpg")){
+el.src = "image/green1r1.jpg";
+} else if (el.src.match("image/green1r1.jpg")){
+el.src = "image/green1r2.jpg";
+} else if (el.src.match("image/green1r2.jpg")){
+el.src = "image/green1r3.jpg";
+} else if (el.src.match("image/green1r3.jpg")){
+el.src = "image/green1.jpg";
+} else {
+}
+}
+
+function nextImageG2(el){
+if (el.src.match("image/green2.jpg")){
+el.src = "image/green2r1.jpg";
+} else if (el.src.match("image/green2r1.jpg")){
+el.src = "image/green2r2.jpg";
+} else if (el.src.match("image/green2r2.jpg")){
+el.src = "image/green2r3.jpg";
+} else if (el.src.match("image/green2r3.jpg")){
+el.src = "image/green2.jpg";
+} else {
+}
+}
+
+function nextImageG3(el){
+if (el.src.match("image/green3.jpg")){
+el.src = "image/green3r1.jpg";
+} else if (el.src.match("image/green3r1.jpg")){
+el.src = "image/green3r2.jpg";
+} else if (el.src.match("image/green3r2.jpg")){
+el.src = "image/green3r3.jpg";
+} else if (el.src.match("image/green3r3.jpg")){
+el.src = "image/green3.jpg";
+} else {
+}
+}
